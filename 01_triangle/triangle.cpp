354cc735b7b48884d46cd32a92b2a0b6d73c8edd
jem <jem.tatha@gmail.com>
2018-12-02 09:55:27 +0800

    LoadShader

--- 01_triangle/triangle.cpp
+++ 01_triangle/triangle.cpp
@@ -5,13 +5,59 @@
 #include <GLFW/glfw3.h>
 #include <stdlib.h>
 
+#include "LoadShaders.h"
+
 using namespace std;
+
+#define BUFFER_OFFSET(a) ((void*)(a))
+
+enum VAO_IDs { Triangles, NumVAOs };
+enum Buffer_IDs { ArrayBuffer, NumBuffers };
+enum Attrib_IDs { vPosition = 0 };
+
+GLuint  VAOs[NumVAOs];
+GLuint  Buffers[NumBuffers];
+
+const GLuint  NumVertices = 6;
+
+void
+init( void )
+{
+    glGenVertexArrays( NumVAOs, VAOs );
+    glBindVertexArray( VAOs[Triangles] );
+
+    GLfloat  vertices[NumVertices][2] = {
+        { -0.90f, -0.90f }, {  0.85f, -0.90f }, { -0.90f,  0.85f },  // Triangle 1
+        {  0.90f, -0.85f }, {  0.90f,  0.90f }, { -0.85f,  0.90f }   // Triangle 2
+    };
+
+    glCreateBuffers( NumBuffers, Buffers );
+    glBindBuffer( GL_ARRAY_BUFFER, Buffers[ArrayBuffer] );
+    glBufferStorage( GL_ARRAY_BUFFER, sizeof(vertices), vertices, 0);
+
+    ShaderInfo  shaders[] =
+    {
+        { GL_VERTEX_SHADER, "triangles.vert" },
+        { GL_FRAGMENT_SHADER, "triangles.frag" },
+        { GL_NONE, NULL }
+    };
+
+    GLuint program = LoadShaders( shaders );
+    glUseProgram( program );
+
+    glVertexAttribPointer( vPosition, 2, GL_FLOAT,
+                           GL_FALSE, 0, BUFFER_OFFSET(0) );
+    glEnableVertexAttribArray( vPosition );
+}
 
 void display()
 {
-	static const float red[] = {1.0f, 0.0f, 0.0f, 0.0f};
-	glClearBufferfv(GL_COLOR, 0, red);
+    static const float black[] = { 0.0f, 0.0f, 0.0f, 0.0f };
 
+    glClearBufferfv(GL_COLOR, 0, black);
+
+    glBindVertexArray( VAOs[Triangles] );
+    glDrawArrays( GL_TRIANGLES, 0, NumVertices );
 }
 
 int main()
@@ -25,6 +71,8 @@ int main()
     glfwMakeContextCurrent(window);
 
 	gl3wInit();
+
+	init();
 
     while (!glfwWindowShouldClose(window))
     {
